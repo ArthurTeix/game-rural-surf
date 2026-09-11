a3b4a8c7857426770b13055cc40b22255113ab36
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-09-11 09:44:07 -0300

    criação de métodos e refatoração do elemento obstaculo

--- game/elements/obstaculo.py
+++ game/elements/obstaculo.py
@@ -1,5 +1,4 @@
 import pygame
-from pathlib import Path
 from random import randint
 
 
@@ -26,31 +25,76 @@ class Obstaculo:
 class GerenciadorObstaculos:
     EVENTO_CRIAR_OBSTACULO = pygame.USEREVENT + 1
 
-    def __init__(self, altura_tela, largura=80, altura=40, velocidade=1, intervalo_ms=700):
+    def __init__(
+        self,
+        largura_tela,
+        altura_tela,
+        largura=80,
+        altura=40,
+        velocidade=3,
+        intervalo_ms=1400
+    ):
+        self.largura_tela = largura_tela
         self.altura_tela = altura_tela
         self.largura = largura
         self.altura = altura
         self.velocidade = velocidade
-
         self.lista_obstaculos = []
 
-        # criar obstáculos periodicamente
-        pygame.time.set_timer(self.EVENTO_CRIAR_OBSTACULO, intervalo_ms)
+        # Avisa o pygame para disparar um evento automaticamente
+        pygame.time.set_timer(
+            self.EVENTO_CRIAR_OBSTACULO,
+            intervalo_ms
+        )
 
     def criar_obstaculo(self):
-        x = randint(15, 405)  # obstáculos nascem aleatoriamente entre o px 15 e 405 de largura
-        novo_obstaculo = Obstaculo(x, -self.altura, self.largura, self.altura)
-        self.lista_obstaculos.append(novo_obstaculo)
+        metade_da_tela = self.largura_tela // 2
+        margem = 15
+
+        x_do_obstaculo_esquerdo = randint(
+            margem,
+            metade_da_tela - self.largura
+        )
+
+        x_do_obstaculo_direito = randint(
+            metade_da_tela,
+            self.largura_tela - self.largura - margem
+        )
+
+        obstaculo_esquerdo = Obstaculo(
+            x_do_obstaculo_esquerdo,
+            -self.altura,
+            self.largura,
+            self.altura
+        )
+
+        obstaculo_direito = Obstaculo(
+            x_do_obstaculo_direito,
+            -self.altura,
+            self.largura,
+            self.altura
+        )
+
+        self.lista_obstaculos.append(obstaculo_esquerdo)
+        self.lista_obstaculos.append(obstaculo_direito)
 
     def mover_obstaculos(self):
         for obstaculo in self.lista_obstaculos:
             obstaculo.mover(self.velocidade)
 
-        # remove obstáculos que já saíram da tela
-        self.lista_obstaculos[:] = [
-            obstaculo for obstaculo in self.lista_obstaculos
-            if obstaculo.rect.y < self.altura_tela
-        ]
+        # Remove obstáculos que saíram da tela
+        obstaculos_ainda_visiveis = []
+        obstaculos_que_sairam = 0
+
+        for obstaculo in self.lista_obstaculos:
+            if obstaculo.rect.y < self.altura_tela:
+                obstaculos_ainda_visiveis.append(obstaculo)
+            else:
+                obstaculos_que_sairam += 1
+
+        self.lista_obstaculos = obstaculos_ainda_visiveis
+
+        return obstaculos_que_sairam
 
     def desenhar_obstaculos(self, tela):
         for obstaculo in self.lista_obstaculos:
@@ -60,4 +104,5 @@ class GerenciadorObstaculos:
         for obstaculo in self.lista_obstaculos:
             if jogador.rect.colliderect(obstaculo.rect):
                 return True
+
         return False
