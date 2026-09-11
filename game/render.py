2dc821e536d332e7f7a4de7f2848ce25e00fa33f
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-09-11 09:45:24 -0300

    tela de jogo criada

--- game/render.py
+++ game/render.py
@@ -1,6 +1,21 @@
 import pygame
 
 from game.configuracoes import IMG_FUNDOS, LARGURA_TELA, FONTE_PONTOS
+from game.util.cores import cores
 
-def desenhar_inicio_jogo(tela, personagem, moedas, obstáculos, fundo, pontos):
-    pass
+def desenhar_jogo(tela, personagem, gerenciador_obstaculos, pontos):
+    # desenha o fundo primeiro, pra ele ficar "atrás" de tudo
+    tela.blit(IMG_FUNDOS[0], (0, 0))
+
+    # desenha o personagem
+    personagem.desenhar(tela)
+
+    # desenha todos os obstáculos que estão na tela
+    gerenciador_obstaculos.desenhar_obstaculos(tela)
+
+    # desenha o texto com a pontuação, no canto superior direito
+    texto_pontos = FONTE_PONTOS.render(f"Pontuação: {pontos}", 1, cores['amarelo'])
+    tela.blit(texto_pontos, (LARGURA_TELA - 10 - texto_pontos.get_width(), 10))
+
+    # atualiza a tela pra mostrar tudo que foi desenhado
+    pygame.display.update()
